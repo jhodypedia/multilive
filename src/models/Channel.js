a860cda8a59e670d06cdae0fc2c3f5eb832298e4
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-04 17:45:30 +0700

    Update Channel.js

--- src/models/Channel.js
+++ src/models/Channel.js
@@ -1,15 +1,14 @@
 const { sequelize, DataTypes } = require('../config/sequelize');
 
 const Channel = sequelize.define('Channel', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING(128), allowNull: false },
-  // Contoh RTMP: rtmp://a.rtmp.youtube.com/live2/STREAM_KEY
-  rtmpUrl: { type: DataTypes.STRING(768), allowNull: false },
-  isActive: { type: DataTypes.BOOLEAN, defaultValue: true },
-  videoBitrate: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_VIDEO_BITRATE || '3500k' },
-  audioBitrate: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_AUDIO_BITRATE || '160k' },
-  resolution:  { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_RESOLUTION || '1280x720' },
-  framerate:   { type: DataTypes.INTEGER, defaultValue: Number(process.env.DEFAULT_FRAMERATE || 30) }
-}, { tableName: 'channels' });
+  id: { type: DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
+  name: { type: DataTypes.STRING, allowNull:false },
+  rtmpUrl: { type: DataTypes.STRING(768), allowNull:false },
+  isActive: { type: DataTypes.BOOLEAN, defaultValue:true },
+  videoBitrate: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_VIDEO_BITRATE },
+  audioBitrate: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_AUDIO_BITRATE },
+  resolution: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_RESOLUTION },
+  framerate: { type: DataTypes.INTEGER, defaultValue: Number(process.env.DEFAULT_FRAMERATE) }
+}, { tableName:'channels' });
 
 module.exports = Channel;
