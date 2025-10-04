dd548c27dfe97431427ab8cf14258fb7e2c53994
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-04 17:46:17 +0700

    Update LiveSession.js

--- src/models/LiveSession.js
+++ src/models/LiveSession.js
@@ -1,14 +1,14 @@
 const { sequelize, DataTypes } = require('../config/sequelize');
 
 const LiveSession = sequelize.define('LiveSession', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  channelId: { type: DataTypes.INTEGER, allowNull: false },
-  status: { type: DataTypes.ENUM('idle','starting','streaming','stopping','stopped','error'), defaultValue: 'idle' },
-  currentItemId: { type: DataTypes.INTEGER, allowNull: true },
-  startedAt: { type: DataTypes.DATE, allowNull: true },
-  stoppedAt: { type: DataTypes.DATE, allowNull: true },
-  message: { type: DataTypes.TEXT, allowNull: true },
-  scheduleId: { type: DataTypes.INTEGER, allowNull: true } // jika dari schedule
-}, { tableName: 'live_sessions' });
+  id: { type: DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
+  channelId: { type: DataTypes.INTEGER, allowNull:false },
+  status: { type: DataTypes.ENUM('idle','starting','streaming','stopped','error'), defaultValue:'idle' },
+  currentItemId: { type: DataTypes.INTEGER },
+  startedAt: { type: DataTypes.DATE },
+  stoppedAt: { type: DataTypes.DATE },
+  message: { type: DataTypes.TEXT },
+  scheduleId: { type: DataTypes.INTEGER }
+}, { tableName:'live_sessions' });
 
 module.exports = LiveSession;
