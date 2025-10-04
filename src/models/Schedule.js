0010b4fc459cd2d9dd597e53172cdd4195730d6c
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-04 17:46:37 +0700

    Update Schedule.js

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,12 +1,11 @@
 const { sequelize, DataTypes } = require('../config/sequelize');
 
 const Schedule = sequelize.define('Schedule', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  channelId: { type: DataTypes.INTEGER, allowNull: false },
-  // Format WIB "YYYY-MM-DD HH:mm:ss"
-  startAtLocal: { type: DataTypes.STRING, allowNull: false },
-  durationMinutes: { type: DataTypes.INTEGER, defaultValue: 60 }, // auto-stop
-  notes: { type: DataTypes.STRING(255) }
-}, { tableName: 'schedules' });
+  id: { type: DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
+  channelId: { type: DataTypes.INTEGER, allowNull:false },
+  startAtLocal: { type: DataTypes.STRING, allowNull:false },
+  durationMinutes: { type: DataTypes.INTEGER, defaultValue:60 },
+  notes: { type: DataTypes.STRING }
+}, { tableName:'schedules' });
 
 module.exports = Schedule;
